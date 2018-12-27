df5717eb9540229cd5e9cd2705402b8a0e536231
pewpewphil <philipo0@Hotmail.com>
2018-12-27 12:47:21 -0500

    adding all files now

---
+++ ReviewProject/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//cout << " hello world "; 
+
+	string testString= "hello world ";
+	int location = testString.size();
+	cout << testString<< "at location " <<location;
+
+
+	return 0; 
+}
