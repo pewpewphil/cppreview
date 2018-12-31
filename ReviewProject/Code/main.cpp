739fd2594ba1d6c47478f2d4e522ff6975966b69
pewpewphil <philipo0@Hotmail.com>
2018-12-31 17:13:05 -0500

    added playing sounds to work now!

--- ReviewProject/Code/main.cpp
+++ ReviewProject/Code/main.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
-#include <string>
-#include <vector> 
-#include <Windows.h>
 //#pragma comment(lib, "winmm.lib")
 
 #include "Headers/BaseClass.h"
 #include "Headers/math.h"
+#include "FmodInc/fmod.h"
+#include "FmodInc/fmod.hpp";
+#include "FmodInc/common.h";
+#include "FmodInc/fmod_errors.h";
 
+#include <iostream>
+#include <string>
+#include <vector> 
+#include <windows.h>
 
 using namespace std;
 
-void TestMathClass();
-
+static unsigned int gPressedButtons = 0;
 
 void TestMathClass()
 {
@@ -48,6 +51,16 @@ void TestStrings()
 	}
 }
 
+const char *Common_MediaPath(const char *fileName)
+{
+	char *filePath = (char *)calloc(256, sizeof(char));
+
+	strcat_s(filePath,256, "media/");
+	strcat_s(filePath,256, fileName);
+	cout << "output=" << filePath << "\n";
+	return filePath;
+}
+
 void TestDervived()
 {
 	Base *base = new Derived1();
@@ -64,13 +77,69 @@ void PlaySoundOld()
 }
 
 
+
 int main()
 {
 	TestMathClass();
-	TestStrings();
-	TestDervived();
-	PlaySoundOld();
+	//TestStrings();
+	//TestDervived();
+	//PlaySoundOld();
+	
+	FMOD::System     *system;
+	FMOD::Sound      *sound1, *sound2, *sound3;
+	FMOD::Channel    *channel = 0;
+	FMOD_RESULT       result;
+	unsigned int      version;
+	void             *extradriverdata = 0;
+	static unsigned int inputButton = 0;
+	static unsigned int ExitPress = 27;
 
-	system("pause");
+	/*
+		Create a System object and initialize
+	*/
+	result = FMOD::System_Create(&system);
+
+	result = system->getVersion(&version);
+	cout << "result is "<< FMOD_ErrorString(result)<<"\n";
+	//ERRCHECK(result);
+
+	if (version < FMOD_VERSION)
+	{
+		printf("FMOD lib version %08x doesn't match header version %08x", version, FMOD_VERSION);
+	}
+
+	result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+	cout << "result is "<< FMOD_ErrorString(result) << "\n";
+
+
+	result = system->createSound(Common_MediaPath("drumloop.wav"), FMOD_DEFAULT, 0, &sound1);
+	//Common_MediaPath("drumloop.wav");
+	cout << "result is "<< FMOD_ErrorString(result) << "\n";
+
+	//result = sound1->setMode(FMOD_LOOP_OFF);    /* drumloop.wav has embedded loop points which automatically makes looping turn on, */
+           /* so turn it off here.  We could have also just put FMOD_LOOP_OFF in the above CreateSound call. */
+
+	do
+	{
+		int channelsplaying = 0;
+		if (inputButton == 1)
+		{
+			result = system->playSound(sound1, 0, false, &channel);
+			cout << "result is " << FMOD_ErrorString(result) << "\n";
+		}
+
+			
+		if (channel)
+		{
+			system->getChannelsPlaying(&channelsplaying, NULL);
+		}
+
+		cin >> inputButton;
+		system->update();
+		Sleep(10);
+	} while (inputButton != 2);
+	
+	result = sound1->release();
+	//system("pause");
 	return 0; 
 }
